e514550ec363cb0a41b2953856384436ee6795ef
Apolinário from multimodal AI art <multimodalart@users.noreply.huggingface.co>
2024-09-03 23:30:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -126,8 +126,8 @@ css='''
 #strip, #gif{min-height: 50px}
 '''
 examples = [["winter", "summer", 1.25, "a dog in the park"], ["USA suburb", "Europe", 2, "a house"], ["rotten", "super fresh", 2, "a tomato"]]
-image_seq = gr.Image(label="Strip", elem_id="strip")
-output_image = gr.Image(label="Gif", elem_id="gif")
+image_seq = gr.Image(label="Strip", elem_id="strip", height=50)
+output_image = gr.Image(label="Gif", elem_id="gif", height=50)
 post_generation_image = gr.Image(label="Generated Images")
 post_generation_slider = gr.Slider(minimum=-10, maximum=10, value=0, step=1)
 seed = gr.Slider(minimum=0, maximum=MAX_SEED, step=1, label="Seed", interactive=True, randomize=True)
@@ -152,13 +152,7 @@ with gr.Blocks(css=css) as demo:
             prompt = gr.Textbox(label="Prompt", info="Describe what you to be steered by the directions", placeholder="A dog in the park")
             x = gr.Slider(minimum=0, value=1.5, step=0.1, maximum=4.0, label="Strength", info="maximum strength on each direction (unstable beyond 2.5)")
             submit = gr.Button("Generate directions")
-            gr.Examples(
-                examples=examples,
-                inputs=[concept_1, concept_2, x, prompt],
-                fn=generate,
-                outputs=[x_concept_1, x_concept_2, avg_diff_x, output_image, image_seq, total_images, post_generation_image, post_generation_slider, seed],
-                cache_examples="lazy"
-            )
+            
         with gr.Column():
             with gr.Group(elem_id="group"):
                 post_generation_image.render()
@@ -175,15 +169,24 @@ with gr.Blocks(css=css) as demo:
         steps = gr.Slider(label = "num inference steps", minimum=1, value=3, maximum=8, step=1)
         interm_steps = gr.Slider(label = "num of intermediate images", minimum=3, value=21, maximum=65, step=2)
         guidance_scale = gr.Slider(
-                label="Guidance scale",
-                minimum=0.1,
-                maximum=10.0,
-                step=0.1,
-                value=3.5,
-            )
+            label="Guidance scale",
+            minimum=0.1,
+            maximum=10.0,
+            step=0.1,
+            value=3.5,
+        )
         randomize_seed = gr.Checkbox(True, label="Randomize seed")
         seed.render()
-     
+
+    examples_gradio = gr.Examples(
+                examples=examples,
+                inputs=[prompt, concept_1, concept_2, x],
+                fn=generate,
+                outputs=[x_concept_1, x_concept_2, avg_diff_x, output_image, image_seq, total_images, post_generation_image, post_generation_slider, seed],
+                cache_examples="lazy"
+            )
+            examples_gradio.dataset._components = [concept_1, concept_2, prompt]
+
     submit.click(fn=generate,
                      inputs=[concept_1, concept_2, x, prompt, randomize_seed, seed, recalc_directions, iterations, steps, interm_steps, guidance_scale, x_concept_1, x_concept_2, avg_diff_x, total_images],
                      outputs=[x_concept_1, x_concept_2, avg_diff_x, output_image, image_seq, total_images, post_generation_image, post_generation_slider, seed])
